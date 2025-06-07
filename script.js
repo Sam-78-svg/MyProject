5b6afca53ba2c333b7bf108409caea72c7eda949
sam-78-svg <kendresangamwaman@gmail.com>
2025-06-08 00:29:44 +0530

    hi

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ function openPP(){
         }
     });
 }
+const link=document.getElementById("resLink");
+link.addEventListener("click", function(event) {
+    const closeBtn = document.getElementById("closePopup");
+    const display2 = document.getElementById("registerPopup");
+
+    display2.style.display = "flex";
+    
+    closeBtn.addEventListener("click", function() {
+        display2.style.display = "none";
+    });
+
+    const display = document.getElementById("login-form");
+    display.style.display="none";
+
+    window.addEventListener("click", function(event) {
+        if (event.target === display2) {
+            display2.style.display = "none";
+        }
+    });
+});
+
+
+
+
+function openLogin(){
+    const openBtn1 = document.getElementById("n-btn2");
+    const closeBtn = document.getElementById("closePopup2");
+    const display = document.getElementById("login-form");
+
+    openBtn1.addEventListener("click", function() {
+        display.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", function() {
+        display.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target === display) {
+            display.style.display = "none";
+        }
+    });
+}
